c964152639a674c20367287dee7f16cf3b8d2d7f
mostafa <mostafa>
2022-04-29 12:44:33 +0300

    test    ddfdgd

---
+++ Main.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# Driver = webdriver.Chrome(executable_path="C:\\bin\\chromedriver.exe")
+Driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+
+# Browser Action 1 > Open Web
+Driver.get("https://google.com")
+sleep(2)
+
+# cpmmmmm
+
+# # Browser Action 2 > print Title
+# Window_title = Driver.title
+# print(Window_title)
+#
+# # Browser Action 3 > Open Web
+# Driver.get("http://wikipedia.com")
+# sleep(2)
+# Driver.back()
+# sleep(2)
+# Driver.forward()
+# sleep(2)
+# Driver.refresh()
+# sleep(2)
+
+Driver.switch_to.new_window("tab")
+Driver.switch_to.new_window('window')
+sleep(3)
